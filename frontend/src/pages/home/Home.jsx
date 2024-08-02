9736c3ec286582620a4fe60616e85297d7988de5
shilpipriya06 <139982930+shilpipriya06@users.noreply.github.com>
2024-08-02 23:31:50 +0530

    second commit
    frontend in making

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>
+  return 
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
+    backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <Sidebar/>
+    
       
     </div>
-  )
+  
 }
 
 export default Home
